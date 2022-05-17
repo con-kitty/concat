5242d689cea4b21c5a240f4e3cdb080187e7bcdf
Ian-Woo Kim <ianwookim@gmail.com>
2022-05-17 10:01:38 -0700

    use parse-cabal-project.

--- flake.nix
+++ flake.nix
@@ -5,51 +5,64 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        haskellLib = (import nixpkgs { inherit system; }).haskell.lib;
 
-        haskellOverlay = self: super: {
-          "concat-inline" = pkgs.haskell.lib.dontHaddock
-            (self.callCabal2nix "concat-inline" ./inline { });
-          "concat-plugin" = self.callCabal2nix "concat-plugin" ./plugin { };
-          "concat-classes" = self.callCabal2nix "concat-classes" ./classes { };
-          "concat-satisfy" = self.callCabal2nix "concat-satisfy" ./satisfy { };
-          "concat-known" = self.callCabal2nix "concat-known" ./known { };
-          #"concat-hardware" = self.callCabal2nix "concat-hardware" ./hardware { };
-          "concat-graphics" =
-            self.callCabal2nix "concat-graphics" ./graphics { };
-          "concat-examples" = pkgs.haskell.lib.dontHaddock
-            (self.callCabal2nix "concat-examples" ./examples { });
-        };
-
-        newHaskellPackages =
-          pkgs.haskellPackages.override { overrides = haskellOverlay; };
-
-      in {
-        packages = {
-          inherit (newHaskellPackages)
-            concat-inline concat-plugin concat-classes concat-satisfy
-            concat-known
-            #concat-hardware
-            concat-graphics concat-examples;
-        };
+        parseCabalProject = import ./parse-cabal-project.nix;
+        concatPackages = let
+          excluded = [ "concat-hardware" ];
+          parsed = parseCabalProject ./cabal.project;
+        in builtins.filter ({ name, ... }: !(builtins.elem name excluded))
+        parsed;
+        concatPackageNames = builtins.map ({ name, ... }: name) concatPackages;
+        haskellOverlay = self: super:
+          builtins.listToAttrs (builtins.map ({ name, path }: {
+            inherit name;
+            value = self.callCabal2nix name (./. + "/${path}") { };
+          }) concatPackages);
 
         # see these issues and discussions:
         # - https://github.com/NixOS/nixpkgs/issues/16394
         # - https://github.com/NixOS/nixpkgs/issues/25887
         # - https://github.com/NixOS/nixpkgs/issues/26561
         # - https://discourse.nixos.org/t/nix-haskell-development-2020/6170
-        overlay = final: prev: {
+        fullOverlay = final: prev: {
           haskellPackages = prev.haskellPackages.override (old: {
             overrides =
               final.lib.composeExtensions (old.overrides or (_: _: { }))
               haskellOverlay;
           });
         };
+      in {
+        overlay = fullOverlay;
 
-        devShell = let
-          hsenv = pkgs.haskellPackages.ghcWithPackages (p: [ p.cabal-install ]);
-        in pkgs.mkShell { buildInputs = [ hsenv ]; };
+        devShells = let
+          mkDevShell = ghcVer:
+            let
+              overlayGHC = final: prev: {
+                haskellPackages = prev.haskell.packages.${ghcVer};
+              };
+
+              newPkgs = import nixpkgs {
+                # Here we use the full overlays from this flake, but the categorifier-*
+                # packages will not be provided in the shell. The overlay is only used
+                # to extract dependencies.
+                overlays = [ overlayGHC fullOverlay ];
+                inherit system;
+              };
+
+            in newPkgs.haskellPackages.shellFor {
+              packages = ps: builtins.map (name: ps.${name}) concatPackageNames;
+              buildInputs = [ newPkgs.haskellPackages.cabal-install ] ++
+                # haskell-language-server on GHC 9.2.1 is broken yet.
+                newPkgs.lib.optional (ghcVer != "ghc921")
+                [ newPkgs.haskell-language-server ];
+              withHoogle = false;
+            };
+        in {
+          "ghc8107" = mkDevShell "ghc8107";
+          "ghc921" = mkDevShell "ghc921";
+        };
       });
 }
